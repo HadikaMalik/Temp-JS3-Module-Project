9805f19bcd18ffea3f671cc66e07e6aa6196953d
Your Name <you@example.com>
2024-02-21 12:18:52 +0000

    dropdown menu added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  state.allEpisodes = allEpisodes;
   makePageForEpisodes(allEpisodes);
 }
 
@@ -21,5 +22,54 @@ function makePageForEpisodes(episodeList) {
 function padNumbers(num) {
   return String(num).padStart(2, '0');
 }
+
+// level 200 
+
+const state = {
+  allEpisodes: [],
+  searchTerm: ""
+};
+
+function render() {
+  const filteredEpisodes = state.allEpisodes.filter(function (episode) {
+    return episode.name.toLowerCase().includes(state.searchTerm.toLowerCase()) || episode.summary.toLowerCase().includes(state.searchTerm.toLowerCase());
+  });
+
+  makePageForEpisodes(filteredEpisodes);
+
+  document.getElementById("search-info").textContent = `Displaying ${filteredEpisodes.length} / 73 episodes`;
+};
+
+const input = document.querySelector("#q");
+
+input.addEventListener("input", function () {
+  state.searchTerm = input.value.toLowerCase();
+  render();
+});
+
+const dropDownMenu = document.getElementById("select");
+dropDownMenu.addEventListener("change", function () {
+  const selectedValue = dropDownMenu.value;
+
+  if (selectedValue === "all") {
+    render(state.allEpisodes);
+  }
+  else {
+    const selectedEpisode = state.allEpisodes.find((episode) => episode.id === parseInt(selectedValue));
+    render([selectedEpisode]);
+    navigateToEpisode(selectedEpisode);
+  }
+});
+
+function navigateToEpisode(episode) {
+  if (episode) {
+    const episodeElement = document.getElementById(`episode - ${episode.id}`);
+
+    if (episodeElement) {
+      episodeElement.scrollIntoView({behavior: "smooth"});
+    }
+  }
+}
+
 
 window.onload = setup;
